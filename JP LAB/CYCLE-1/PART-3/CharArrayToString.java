30f5233db1bc77d205b146475033971631cc815d
19wh1a1245 <69250973+19wh1a1245@users.noreply.github.com>
2021-07-25 18:48:43 +0530

    Add files via upload

---
+++ JP LAB/CYCLE-1/PART-3/CharArrayToString.java
@@ -0,0 +1,14 @@
+public class CharArrayToString
+{  
+public static void main(String args[])  
+{  
+ 
+char[] chars = {'j', 'a', 'v', 'a', ' ', 'l', 'a', 'b'};  
+  
+String str = new String();  
+
+String string = str.valueOf(chars);  
+
+System.out.println(string);  
+}  
+}  
